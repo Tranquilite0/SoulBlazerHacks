db19a72bd877e4da0eb1e8e8daff9c62eaaeaff8
Tranquilite <2297233+Tranquilite0@users.noreply.github.com>
2023-11-23 10:43:28 -0700

    Added semiprogressive and text speed hacks.

---
+++ semiprogressive.asm
@@ -0,0 +1,192 @@
+; Semiprogressive Swords/Armor
+; Str/Def scales off of the number of Swords/Armors Obtained while unique effects are still tied to their respective item.
+
+arch 65816
+lorom
+
+check title "SOULBLAZER - 1 USA   "
+
+; 7A57 to 7FBF is Unused. Lets put our hacks there.
+org $00FA57
+
+; Hook Into the drawing Function that draws Swords/Shields (and lots of other stuff too)
+; Address to load is in A
+DrawHook:
+CMP #$0000
+BEQ .weapon
+CMP #$0001
+BEQ .armor
+; Not our magic numbers, run original code and return
+TAY
+LDA $0000,Y
+RTL
+.weapon
+JSR CalcSwords
+RTL
+.armor
+JSR CalcShields
+RTL
+
+GiveItemHook:
+; Original Code that was replaced
+DEY
+STA $1B1E,Y
+; This is a copy of the routine that sets STR/DEF after equiping a sword/armor (I hope it works here)
+LDX $1B5E
+JSL CalcStrength
+STA $1B70
+LDA $02E1BD,X
+STA $1B74
+LDX $1B60
+LDA $02E1AC,X
+CLC
+ADC $1B70
+STA $1B70
+JSL CalcDefense
+CLC
+ADC $1B74
+STA $1B74
+RTL
+
+
+; Calculates strength based off of the number of swords obtained
+CalcStrength:
+PHP ; TODO: Figure out which registers dont need to be saved/restored
+PHX
+PHY
+REP #$10
+SEP #$20
+LDA $1B5E ; Check current sword
+BEQ .end  ; Not Equipped = 0 Strength
+LDY #$1B1E ; Address of Swords Inventory
+JSR CountSwordArmor
+TAX
+LDA $02E1C5,X ; User the number of obtained swords as the index into the sword power table
+.end:
+PLY
+PLX
+PLP
+RTL
+
+; Like CalcStrength, but not a long subroutine and doesnt check equipped item
+CalcSwords:
+PHP
+PHX
+PHY
+REP #$10
+SEP #$20
+LDY #$1B1E ; Address of Swords Inventory
+JSR CountSwordArmor
+TAX
+LDA $02E1C5,X ; User the number of obtained swords as the index into the sword power table
+PLY
+PLX
+PLP
+RTS
+
+; Calculates defense based off of the number of armors obtained
+CalcDefense:
+PHP
+PHX
+PHY
+REP #$10
+SEP #$20
+LDA $1B60 ; Check current armor
+BEQ .end  ; Not Equipped = 0 Defense
+LDY #$1B26 ; Address of Armor Inventory
+JSR CountSwordArmor
+TAX
+LDA $02E1AC,X ; User the number of obtained armors as the index into the armor defense table
+.end:
+PLY
+PLX
+PLP
+RTL
+
+; Like CalcDefense, but not a long subroutine and doesnt check equipped item
+CalcShields:
+PHP
+PHX
+PHY
+REP #$10
+SEP #$20
+LDY #$1B26 ; Address of Armor Inventory
+JSR CountSwordArmor
+TAX
+LDA $02E1AC,X ; User the number of obtained armors as the index into the armor defense table
+PLY
+PLX
+PLP
+RTS
+
+CountSwordArmor:
+LDX #$0008 ; Loop 8 Times
+LDA #$00 ; Number of counted items, in B
+XBA
+.loop:
+LDA $0000,Y
+AND #$7F ; Equipped items have the highest bit set
+BEQ .skip
+XBA
+INC
+XBA
+.skip:
+INY
+DEX
+BNE .loop
+LDA #$00
+XBA
+RTS
+
+
+org $029EF0
+JSL CalcStrength
+
+org $029F0C
+JSL CalcDefense
+
+org $02A837
+JSL DrawHook
+
+org $02A105
+JSL GiveItemHook
+
+; Edit the Weapon Table to modify the strength pointers
+org $02D60F
+dw $0000
+org $02D63C
+dw $0000
+org $02D67A
+dw $0000
+org $02D6A8
+dw $0000
+org $02D6EB
+dw $0000
+org $02D720
+dw $0000
+org $02D75D
+dw $0000
+org $02D79C
+dw $0000
+
+; Edit the Armor Table to modify the defense pointers
+org $02D7CC
+dw $0001
+org $02D806
+dw $0001
+org $02D838
+dw $0001
+org $02D864
+dw $0001
+org $02D895
+dw $0001
+org $02D8CA
+dw $0001
+org $02D906
+dw $0001
+org $02D935
+dw $0001
+
+; Edit the Level Requirement Table (word, BCD)
+org $02E1CE
+dw $0001, $0001, $0001, $0001, $0001, $0001, $0001, $0001
---
+++ textspeed.asm
@@ -0,0 +1,66 @@
+; Text Speed Hacks
+; 3 - US version normal text speed
+; 2 - JP version normal text speed
+; 0 - Instant (can break the game if used some places, not sure why)
+
+arch 65816
+lorom
+
+check title "SOULBLAZER - 1 USA   "
+
+; For text speeds that can be any speed
+!text_speed = $00
+; For text speed that cannot be instant (0x00)
+!text_speed_not_instant = $01
+
+; General text speed set at game start. Covers 90 percent of text boxes.
+org $04F96C
+db !text_speed
+
+; Dr Leo before he explodes slow text start.
+org $04DF0F
+db !text_speed
+
+; Dr Leo after exploding text speed restore.
+org $04DF19
+db !text_speed
+
+; Dr Leo before death slow text start.
+org $04E004
+db !text_speed
+
+; Dr Leo post death text speed restore.
+org $04E00E
+db !text_speed
+
+; In Green Wood, when "you" are on the menu.
+org $03BA48
+db !text_speed_not_instant
+
+; Post deathtoll defeat. Begin Epilogue autotext.
+;org $00D3AF
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. For speech given by Master.
+;org $03B356
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. Lisa talks with Turbo the Goat.
+;org $03B38F
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. Lisa sees Hero.
+;org $03B3FD
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. Restore Text Speed right after.
+;org $03B407
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. Turbo the Goat goes Bleeeet.
+;org $03B45C
+;db !text_speed_not_instant
+
+; Post Credits Text Speed change. Restore Text Speed right after.
+;org $03B466
+;db !text_speed_not_instant
